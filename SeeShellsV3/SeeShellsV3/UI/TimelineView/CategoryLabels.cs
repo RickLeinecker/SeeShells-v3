61e4af85acb06047b257ea5c443e90ac655d13f0
wesnewcomb <newcombw@knights.ucf.edu>
2023-03-31 09:41:48 -0400

    Added a class to allow the updateLabels method to be called.

---
+++ SeeShellsV3/SeeShellsV3/UI/TimelineView/CategoryLabels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeShellsV3.UI
+{
+    public class CategoryLabels : OxyPlot.Axes.CategoryAxis
+    {
+        public void update(int numOfCategories)
+        {
+            this.UpdateLabels(numOfCategories);
+        }
+    }
+}
